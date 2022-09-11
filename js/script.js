7ccf8795b1877a8c6181f7d8f5cea700bdfc6e2c
Mateusz <106282318+mchorosinski@users.noreply.github.com>
2022-09-12 01:32:40 +0300

    Complete function: titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log('Argument "event" funkcji (in. handler) "titleClickHandler": ', event);
@@ -13,7 +14,7 @@ const titleClickHandler = function (event) {
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
@@ -27,11 +28,21 @@ const titleClickHandler = function (event) {
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector:', articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle:', targetArticle);
+
+  /* [IN PROGRESS] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+  console.log('Active article:', targetArticle);
+
 // eslint-disable-next-line semi
 }
 
